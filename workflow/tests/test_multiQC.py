8e0ffe7c663005fe851a02a205929069314f0550
s181706 <jonathan.gesell@utsouthwestern.edu>
2019-05-10 13:33:49 -0500

    Updated testing scripts with correct values

--- workflow/tests/test_multiQC.py
+++ workflow/tests/test_multiQC.py
@@ -8,20 +8,20 @@ test_output_mc_path = os.path.dirname(os.path.abspath(__file__)) + '/../QC/Raw/S
 @pytest.mark.multiqchtml
 def test_html():
 	assert  os.path.exists(os.path.join(test_output_path, "SRADownload.MultiQC.Report.html"))
-	assert  os.path.getsize(os.path.join(test_output_path, "SRADownload.MultiQC.Report.html")) == 1343661
+	assert  os.path.getsize(os.path.join(test_output_path, "SRADownload.MultiQC.Report.html")) == 1280320
 
 @pytest.mark.multiqcfiles
 def test_files():
 	assert  os.path.exists(os.path.join(test_output_mc_path, "multiqc_data.json"))
-	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_data.json")) == 1067025
+	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_data.json")) == 754504
 
 	assert  os.path.exists(os.path.join(test_output_mc_path, "multiqc_fastqc.txt"))
-	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_fastqc.txt")) == 3447
+	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_fastqc.txt")) == 2610
 
 	assert  os.path.exists(os.path.join(test_output_mc_path, "multiqc_general_stats.txt"))
-	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_general_stats.txt")) == 1223
+	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_general_stats.txt")) == 1040
 
 	assert  os.path.exists(os.path.join(test_output_mc_path, "multiqc.log"))
-	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc.log")) == 27237
+	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc.log")) == 27463
 	assert  os.path.exists(os.path.join(test_output_mc_path, "multiqc_sources.txt"))
-	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_sources.txt")) == 2366
+	assert  os.path.getsize(os.path.join(test_output_mc_path, "multiqc_sources.txt")) == 1674
