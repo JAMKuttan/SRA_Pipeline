f771ac7ed402e9f3d1e27fcce69d934ffb3e0124
Jonathan Gesell <jonathan.gesell@utsouthwestern.edu>
2019-06-10 14:05:20 -0500

    Update test_download_sras.py

--- workflow/tests/test_download_sras.py
+++ workflow/tests/test_download_sras.py
@@ -7,33 +7,33 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + '/../Samples/'
 @pytest.mark.downloads
 def test_download_sefastq():
 	assert  os.path.exists(os.path.join(test_output_path, "Test5.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test5.fastq.gz")) == 228317277
+	assert  os.path.getsize(os.path.join(test_output_path, "Test5.fastq.gz")) >= 228000000
 
 	assert  os.path.exists(os.path.join(test_output_path, "Test6.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test6.fastq.gz")) == 115030000
+	assert  os.path.getsize(os.path.join(test_output_path, "Test6.fastq.gz")) >= 115000000
 
 	assert  os.path.exists(os.path.join(test_output_path, "Test7.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test7.fastq.gz")) == 197268189
+	assert  os.path.getsize(os.path.join(test_output_path, "Test7.fastq.gz")) >= 197000000
 
 	assert  os.path.exists(os.path.join(test_output_path, "Test8.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test8.fastq.gz")) == 113240035
+	assert  os.path.getsize(os.path.join(test_output_path, "Test8.fastq.gz")) >= 113000000
 
 	assert  os.path.exists(os.path.join(test_output_path, "Test1_1.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test1_1.fastq.gz")) == 420568841 
+	assert  os.path.getsize(os.path.join(test_output_path, "Test1_1.fastq.gz")) >= 420000000 
 	assert  os.path.exists(os.path.join(test_output_path, "Test1_2.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test1_2.fastq.gz")) == 429490444
+	assert  os.path.getsize(os.path.join(test_output_path, "Test1_2.fastq.gz")) >= 429000000
 
 	assert  os.path.exists(os.path.join(test_output_path, "Test2_1.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test2_1.fastq.gz")) == 180441860
+	assert  os.path.getsize(os.path.join(test_output_path, "Test2_1.fastq.gz")) >= 180000000
 	assert  os.path.exists(os.path.join(test_output_path, "Test2_2.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test2_2.fastq.gz")) == 182791001
+	assert  os.path.getsize(os.path.join(test_output_path, "Test2_2.fastq.gz")) >= 182000000
 
 	assert  os.path.exists(os.path.join(test_output_path, "Test3_1.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test3_1.fastq.gz")) == 420245128
+	assert  os.path.getsize(os.path.join(test_output_path, "Test3_1.fastq.gz")) >= 420000000
 	assert  os.path.exists(os.path.join(test_output_path, "Test3_2.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test3_2.fastq.gz")) == 429777352
+	assert  os.path.getsize(os.path.join(test_output_path, "Test3_2.fastq.gz")) >= 429000000
 
 	assert  os.path.exists(os.path.join(test_output_path, "Test4_1.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test4_1.fastq.gz")) == 139474358
+	assert  os.path.getsize(os.path.join(test_output_path, "Test4_1.fastq.gz")) >= 139000000
 	assert  os.path.exists(os.path.join(test_output_path, "Test4_2.fastq.gz"))
-	assert  os.path.getsize(os.path.join(test_output_path, "Test4_2.fastq.gz")) == 142930243
+	assert  os.path.getsize(os.path.join(test_output_path, "Test4_2.fastq.gz")) >= 142000000
